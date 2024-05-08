1bb7b6a922aa1c1ea54860874b60b7bc3c2eb7bc
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2024-05-08 06:55:59 +0400

    upd

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {CustomSelect} from './modules/select/custom-select';
 import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
+import {initHeader} from './modules/header/init-header.js';
 
 // ---------------------------------
 
@@ -15,6 +16,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   // ---------------------------------
+  initHeader();
+
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
---
+++ source/js/modules/header/init-header.js
@@ -0,0 +1,6 @@
+import {Burger} from './burger';
+
+export function initHeader() {
+  const burger = new Burger();
+  burger.init();
+}
